8e13b6b2e5773562c118220611bc45f19702cfd3
Yankovsky Andrey <YankovskyAndrey@gmail.com>
2013-02-15 04:15:28 +0400

    another try

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-a 
+<!DOCTYPE HTML>
+<html lang="en">
+    <head>
+        <title>spront</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <label>Введите текст на кириллице:
+            <input type="text" onkeyup="handleInputChange(this)">
+        </label>
+        Удалите всё чтобы отчистить вывод
+        <div>
+            <canvas id="myText" width="1400"></canvas>
+        </div>
+        <script type="text/javascript" src="casprifont.js"></script>
+    </body>
+</html>
